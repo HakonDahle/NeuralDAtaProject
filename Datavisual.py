8e81521947c1a9717f55699d29244ae3a39042cf
eliaskvamme <eliaskvamme@outlook.com>
2021-09-30 11:04:41 +0200

    La til punktum

--- Datavisual.py
+++ Datavisual.py
@@ -5,7 +5,7 @@ import pycxsimulator
 from pylab import *
 
 
-# Data for reading the file and putting each line into a list
+# Data for reading the file and putting each line into a list.
 def readFile(fileName):
         fileObj = open(fileName, "r") #opens the file in read mode
         words = fileObj.read().splitlines() #puts the file into an array
