da9500d68dc191736adca4cbd17fe02a874e507d
eliaskvamme <eliaskvamme@outlook.com>
2021-10-01 14:18:26 +0200

    endra noe som har komt dobbelt

--- ListsAndFeatures.py
+++ ListsAndFeatures.py
@@ -43,7 +43,7 @@ def readFile(fileName):
         fileObj.close()
         return words
 
-dense2310 = readFile('C:\Dense 2-3-10.spk.txt') # Loads the data
+dense2310 = readFile('data\Dense 2-3-10.spk.txt') # Loads the data
 t = [i.split()[0] for i in dense2310][:]    # Copies the data list and stores the time into a separate list
 data = [i.split()[1] for i in dense2310][:] # Copies the data list and stores the data into a separate list
 width = 8   # Number of electrodes in x direction
@@ -54,19 +54,6 @@ for i in range(len(electrodes)):
     if len(electrodes[i]) == 0:
         print(electrodes[i],i)
 
-def electrode_list(activeElectrodes,ti):
-    temp_array = []
-    temp_electrode = []
-    for electrodenr in range(0,64):
-        for element in range(len(activeElectrodes)):
-            spike = int(activeElectrodes[element][:])  # The spiked electrode
-            
-            if electrodenr == spike:
-                temp_electrode.append([element,ti[element]])
-        temp_array.append(temp_electrode[:])
-        temp_electrode.clear()
-    
-    return temp_array
 
 electrodes = electrode_list(data,t)
 print("electrodes[0]: ",electrodes[0][0][1])
