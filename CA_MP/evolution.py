5ecbd3750b8819a54212a60c3272f905037bf09d
eliaskvamme <eliaskvamme@outlook.com>
2021-10-23 11:47:06 +0200

    bestspikes

--- CA_MP/evolution.py
+++ CA_MP/evolution.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
         sim_output.clear()
 
 
-    f = open("Best_spikes.txt", "w")
+    f = open("Bestspikes.txt", "w")
     for element in sim_output[beste_regel]:
         f.write(str(element[0]) + " " + str(element[1]) + '\n')
     f.close()
