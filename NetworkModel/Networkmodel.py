d13df1303f72d2db1cb15ae3c6988f6d976a5372
Hakon Dahle <haakondah@gmail.com>
2021-10-25 12:19:19 +0200

    added multiprocessing

--- NetworkModel/Networkmodel.py
+++ NetworkModel/Networkmodel.py
@@ -1,9 +1,11 @@
+import os
 import random as r
 import copy
 import networkx as nx
 import matplotlib.pyplot as plt
 import load_data
 import time
+from multiprocessing import Pool
 import fitness as fit
 from networkx.classes.reportviews import EdgeDataView
 
@@ -123,6 +125,110 @@ def phenotype_generator(G_,gen,population_size):
         print("phenotype1 len: ",len(phenotype_[1]))'''
 
     return phenotype_          
+
+
+def multi_phenotype_generator(args):
+    G_,gen,population_size = args[0], args[1], args[2]
+    phenotype_temp = []
+    phenotype_ = [[]]*population_size
+    fs = 50000
+    for i in range(population_size):
+        time = 0
+        G_.add_nodes_from(gen[i])
+        #print("i: ",i,"nodes data: ",G_.nodes.data())
+        '''G_.nodes[1]["spike"] = 1
+        G_.nodes[2]["spike"] = 1
+        G_.nodes[0]["potential"] = 0.5
+        G_.edges[0,1]["weight"] = 1
+        G_.edges[0,2]["weight"] = 1'''
+        while time < 1:
+            for nodenr in range(len(gen[i])):
+                #print("1",G_.nodes[nodenr])
+                self_prob = r.random()
+                
+                if G_.nodes[nodenr]["potential"] > 0:   # Checks if the node has a voltage potential to decrease it with the decay constant
+                    #print("1: potential >0:", G_.nodes[nodenr]["potential"])
+                    G_.nodes[nodenr]["potential"] -= G_.nodes[nodenr]["decay constant"]
+                    #print("2: potential >0:", G_.nodes[nodenr]["potential"])
+                elif G_.nodes[nodenr]["potential"] < 0:
+                    G_.nodes[nodenr]["potential"] = 0
+                    #print("potential <0:", G_.nodes[nodenr]["potential"])
+                
+                if G_.nodes[nodenr]["exhausted"] > 0:   # Exhausted nodes are inactive for a period of time
+                    G_.nodes[nodenr]["exhausted"] -= 1/fs
+                    #print("exhausted: ",G_.nodes[nodenr]["exhausted"])
+                
+                elif G_.nodes[nodenr]["exhausted"] == 0:
+                    if G_.nodes[nodenr]["prev spike"] == 1:
+                        G_.nodes[nodenr]["prev spike"] = 0
+
+                    for k, nbrs in G_.adj.items():  # Checks the neighbours for spikes and multiplies it with the weighted edge
+                        if k == i:
+                            for nbr, eattr in nbrs.items():
+                                G_.nodes[nodenr]["potential"] += G_.nodes[nbr]["prev spike"]*eattr["weight"]
+                                #print("nodenr: ",nodenr,"potential: ",G_.nodes[nodenr]["potential"],"nbr: ",nbr,"nbr_prev_spike: ", G_.nodes[nbr]["prev spike"])
+                    if (G_.nodes[nodenr]["potential"] >  G_.nodes[nodenr]["threshold"]) or ((self_prob <= G_.nodes[nodenr]["prob selffire"])):  # Node spikes if threshold is exceeded
+                        G_.nodes[nodenr]["spike"] = 1
+                        #print("time: ",time, "potential: ", G_.nodes[nodenr]["potential"],"threshold: ",G_.nodes[nodenr]["threshold"], "self_prob: ",self_prob,"selffire: ",G_.nodes[nodenr]["prob selffire"])
+                        phenotype_temp.append([float(time),nodenr])
+                        #print("i: ",i,"phenotype_temp: ",phenotype_temp)
+                        
+
+                elif G_.nodes[nodenr]["exhausted"] < 0:
+                    G_.nodes[nodenr]["exhausted"] = 0
+                
+                #print("node: ",nodenr,"potential: ",G_.nodes[nodenr]["potential"],"nbr_spike: ", G_.nodes[nbr]["spike"])
+                #print("2",G_.nodes[nodenr])
+                if G_.nodes[nodenr]["spike"] == 1:  # Resets spikes if there has been a spike in last iteration and deactivates the node
+                    G_.nodes[nodenr]["prev spike"] = 1
+                    G_.nodes[nodenr]["spike"] = 0
+                    G_.nodes[nodenr]["exhausted"] = G_.nodes[nodenr]["obstruction period"]
+                    G_.nodes[nodenr]["potential"] = 0
+                    G_.nodes[nodenr]["exhausted"] = G_.nodes[nodenr]["obstruction period"]
+
+            time += 1/fs
+                
+            '''
+            """
+            Drawing
+            """
+            pos = nx.spring_layout(G_, iterations=300, seed=3977)
+            color_map = []
+            if G_.nodes[nodenr]['prev spike'] == 1:
+                color_map.append('green')
+            elif G_.nodes[nodenr]['exhausted'] > 0:
+                color_map.append('red')
+            else:
+                color_map.append('gray')
+            nx.draw(
+                G_,
+                pos,
+                node_color=color_map,
+                edgecolors="tab:gray",  # Node surface color
+                edge_color="tab:gray",  # Color of graph edges
+                node_size=100,
+                with_labels=True,
+                width=3,
+            )
+            plt.show()'''
+
+        phenotype_[i] = copy.deepcopy(phenotype_temp)
+        #print("phenotype_: ",phenotype_)
+        #print("i: ",i,"phenotype_temp: ",phenotype_temp)
+        phenotype_temp.clear()
+        '''print("phenotype0_: ",phenotype_[0])
+        print("phenotype0 len: ",len(phenotype_[0]))
+        print("phenotype1_: ",phenotype_[1])
+        print("phenotype1 len: ",len(phenotype_[1]))'''
+
+    return phenotype_ 
+
+
+def multiprocessing(G_,gen,population_size):
+    with Pool(os.cpu_count()-1) as p:
+        pheno_type = p.map(multi_phenotype_generator(),gen)
+        p.close()
+    return pheno_type
 
 """
 M U T A T I O N
